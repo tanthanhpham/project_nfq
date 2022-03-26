a75dfb0f9cb0575a1f7b43dc5551f197d98ee78c
Tan Thanh Pham <tanthanh.pham@nfq.asia>
2022-03-27 02:44:24 +0700

    api get all products

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -2,23 +2,34 @@
 
 namespace App\Controller\Api;
 
+use App\Repository\ProductRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends AbstractFOSRestController
 {
+    private $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     /**
      * @Rest\Get("/products")
      */
     public function getProducts(): Response
     {
-        $products = [
-            'name' => 'Product name',
-            'description' => 'Product description'
-        ];
+        $products = $this->productRepository->findAll();
+
+        $serializer = SerializerBuilder::create()->build();
+        $convertToJson = $serializer->serialize($products, 'json', SerializationContext::create()->setGroups('show'));
+        $products = $serializer->deserialize($convertToJson, 'array', 'json');
 
         return $this->handleView($this->view($products));
     }
